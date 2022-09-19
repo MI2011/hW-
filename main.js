a0cfdfca4435d1dfad7d76581124dac8e26510f1
MI2011 <107559126+MI2011@users.noreply.github.com>
2022-09-19 18:45:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550,550);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Inntialized');
+}
+
+function draw() {
+    background('#90E4C1');
+}
+
+function gotPoses(results) 
+{
+    if(results.lenght > 0)
+    {
+        console.log(results);
+    }
+     
+}
